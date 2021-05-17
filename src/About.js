bcaf594b616c0f7f5696946f7fe5569a39c7757d
sandhya-04 <sandy13097@gmail.com>
2021-05-17 11:24:52 +0530

    changes

--- src/About.js
+++ src/About.js
@@ -1,18 +1,11 @@
 import {React} from "react";
 import sandy from "./sandy.jpg";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-    
-    faFacebook,
-    faTwitter,
-    faInstagram,
-    faGithub,
-    faLinkedinIn
-  } from "@fortawesome/free-brands-svg-icons";
+
 
 
 export function About(){
     return (
+      
         <div className="sandhya" id="san1">
             <img  className="san3"
             src={sandy} alt="sandy" height="300" width="300"/>
@@ -23,30 +16,22 @@ export function About(){
                 <div className="main1">
                     <p className="main2"><i><h3>Incoming SDE at Deutsche Bank</h3></i></p>
                     <p className="main2"><i><h3>Ex-SDE Intern at Morgan Stanley</h3></i></p>
-                    <p className="main2"><i><h3>Additional Secretary at CSEA NITW</h3></i></p>                    
+                    <p className="main2"><i><h3>Additional Secretary at CSEA NITW | MCA NITW'21</h3></i></p>                    
                     </div> 
+                    <hr/>
+                    <div className="main4">
+                      <code><h2><b>Education</b></h2></code>
+                    </div>
+                    <div>
+                      <p className="main5"><i><h3>MCA'21 from NIT WARNAGAL</h3></i></p>
+                      <p className="main5"><i><h3>BCA from Agra University</h3></i></p>
+                    </div>
+                    <hr/>
+                  
                    
-                    <div class="social-container">
-      
-      <a href="https://www.facebook.com/Sandhya Singh/"
-  className="facebook social">
-  <FontAwesomeIcon icon={faFacebook} size="2x" />
-</a>
-<a href="https://www.instagram.com/sansin_04"
-  className="instagram social">
-  <FontAwesomeIcon icon={faInstagram} size="2x" />
-</a>
-<a href="https://twitter.com/Sandhya96923018" className="twitter social">
-  <FontAwesomeIcon icon={faTwitter} size="2x" />
-</a>
-<a href="https://www.linkedin.com/in/singhsandhya130197?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BmHl48u6ESMaPqOV5Ctt2PQ%3D%3D" className="linkedin social">
-<FontAwesomeIcon icon={faLinkedinIn} size="2x" />
-
-</a>
-
-    </div>
-                    
-        </div>
+                   
+                    </div>                    
+        
         
     );
 }
